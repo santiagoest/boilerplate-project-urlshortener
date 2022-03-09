ea3a8f26bfd0a223d8d644c5e6b2d1297b07d533
santiagoest <santiagotomasestevez@gmail.com>
2022-03-09 02:19:08 -0300

    finished project

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser');
+const isurl = require('is-url');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const url_map = new Map();
+let count = 0;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(bodyparser.urlencoded([{ extended : false}]));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +24,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const original_url = req.body.url;
+  const short_url = count;
+  if(!isurl(original_url)){
+    res.json({ error: 'invalid url' })
+  }
+  else if (Array.from(url_map.values()).includes(original_url) === false){
+    url_map.set(short_url, original_url);
+    count++;
+  }
+  res.json({original_url, short_url});
+});
+
+app.get('/api/shorturl/:id', function(req, res) {
+  const shorturl_id = parseInt(req.params.id);
+  original_url = url_map.get(shorturl_id);
+  res.redirect(original_url);
 });
 
 app.listen(port, function() {
